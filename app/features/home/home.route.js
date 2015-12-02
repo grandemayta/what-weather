419d44c4fe701941957c0e2a3def0ebeb40de421
Gabriel Mayta <grandemayta@gmail.com>
2015-12-02 23:58:48 +0100

    Application structure is almost ready

--- app/features/home/home.route.js
+++ app/features/home/home.route.js
@@ -17,7 +17,14 @@ function HomeRoute($stateProvider, $urlRouterProvider) {
             url: "/",
             controller: "HomeController",
             controllerAs: "home",
-            template: require("./_views/home.html"),
+            templateProvider: ["$q", function ($q) {
+                var deferred = $q.defer();
+                require.ensure(["./_views/home.html"], function () {
+                    var template = require("./_views/home.html");
+                    deferred.resolve(template);
+                });
+                return deferred.promise;
+            }],
             resolve: ["$q", "$ocLazyLoad", function ($q, $ocLazyLoad) {
                 var deferred = $q.defer();
                 require.ensure([], function (require) {
